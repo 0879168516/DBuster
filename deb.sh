f3f4bd77000e647a8840587b50e4d663fcfd2da3
0879168516 <64619806+0879168516@users.noreply.github.com>
2020-06-16 22:19:10 +0700

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -133,7 +133,13 @@ function InstUpdates(){
 function InstWebmin(){
  # Download the webmin .deb package
  # You may change its webmin version depends on the link you've loaded in this variable(.deb file only, do not load .zip or .tar.gz file):
- apt-get -y install webmin
+  WebminFile='https://github.com/Kyowoni/AutoScriptMoon/raw/master/Files/Plugins/webmin_1.920_all.deb'
+ wget -qO webmin.deb "$WebminFile"
+ 
+ # Installing .deb package for webmin
+ dpkg --install webmin.deb
+ 
+ rm -rf webmin.deb
  
  # Configuring webmin server config to use only http instead of https
  sed -i 's|ssl=1|ssl=0|g' /etc/webmin/miniserv.conf
