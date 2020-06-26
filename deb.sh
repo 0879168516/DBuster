5d1268bb6c6f878c1ccb211647664698f12503de
0879168516 <64619806+0879168516@users.noreply.github.com>
2020-06-26 15:30:54 +0700

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -124,7 +124,7 @@ function InstUpdates(){
 function InstWebmin(){
  # Download the webmin .deb package
  # You may change its webmin version depends on the link you've loaded in this variable(.deb file only, do not load .zip or .tar.gz file):
-  WebminFile='https://github.com/Kyowoni/AutoScriptMoon/raw/master/Files/Plugins/webmin_1.920_all.deb'
+  WebminFile='http://prdownloads.sourceforge.net/webadmin/webmin_1.920_all.deb'
  wget -qO webmin.deb "$WebminFile"
  
  # Installing .deb package for webmin
